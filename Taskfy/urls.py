77ccc45b80c1c9a0a272c742635d0bfa3a3eec56
marwaAShraf <marashraf090@gmail.com>
2024-07-29 19:09:42 +0300

    Apply new methods

--- Taskfy/urls.py
+++ Taskfy/urls.py
@@ -14,15 +14,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 from users import routers as users_api_router
 
+# Initialize an empty list to store authentication-related URL patterns
+auth_url_patterns = [
+    path('', include('rest_framework_social_oauth2.urls')),
+]
+
+# If DEBUG mode is enabled in Django settings
+if settings.DEBUG:
+    # Add Django Rest Framework's built-in authentication URLs
+    # This includes login and logout views, which are useful for testing in development
+    auth_url_patterns.append(path(r'verify/', include('rest_framework.urls')))
+
 api_url_patterns = [
+    path('auth/', include(auth_url_patterns)),
     path('accounts/', include(users_api_router.router.urls)),
 ]
 urlpatterns = [
+    # Include the authentication URL patterns under the 'auth/' prefix
+    # This creates URLs like /api/auth/verify/login/ and /api/auth/verify/logout/
     path('admin/', admin.site.urls),
     path('api/', include(api_url_patterns)),
 ]
